0543fb4062a0f251ea5b30e2f705895eb23a5b34
calebcabob <calphonic@gmail.com>
2018-02-10 18:17:37 -0500

    This is the Age of AquariOS
    Thanks for the help! @MrApocalypse
    
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- crowdin_sync.py
+++ crowdin_sync.py
@@ -3,7 +3,7 @@
 # crowdin_sync.py
 #
 # Updates Crowdin source translations and pushes translations
-# directly to ScrewdAOSP Github.
+# directly to AquariOS Github.
 #
 # Copyright (C) 2014-2015 The CyanogenMod Project
 # This code has been modified. Portions copyright (C) 2016, The PAC-ROM Project
@@ -83,7 +83,7 @@ def push_as_commit(base_path, path, name, branch, username):
 
     # Push commit
     try:
-        repo.git.push('git@github.com:ScrewdAOSP/%s' % (name),
+        repo.git.push('git@github.com:AquariOS/%s' % (name),
                       'HEAD:%s' % branch)
         print('Successfully pushed commit for %s' % name)
     except:
@@ -109,16 +109,16 @@ def find_xml(base_path):
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Synchronising ScrewdAOSP translations with Crowdin")
+        description="Synchronising AquariOS translations with Crowdin")
     sync = parser.add_mutually_exclusive_group()
     parser.add_argument('-u', '--username', help='Gerrit username',
                         required=True)
-    parser.add_argument('-b', '--branch', help='Screwd branch',
+    parser.add_argument('-b', '--branch', help='AquariOS branch',
                         required=True)
     sync.add_argument('--no-upload', action='store_true',
-                      help='Only download ScrewdAOSP translations from Crowdin')
+                      help='Only download AquariOS translations from Crowdin')
     sync.add_argument('--no-download', action='store_true',
-                      help='Only upload ScrewdAOSP source translations to Crowdin')
+                      help='Only upload AquariOS source translations to Crowdin')
     return parser.parse_args()
 
 # ################################# PREPARE ################################## #
@@ -212,7 +212,7 @@ def download_crowdin(base_path, branch, xml, username, no_download=False):
             paths.append(p.replace('/%s' % branch, ''))
 
     print('\nUploading translations to Github')
-    xml_android = load_xml(x='%s/manifest/manifests/screwd.xml' % base_path)
+    xml_android = load_xml(x='%s/manifest/default.xml' % base_path)
     items = xml_android.getElementsByTagName('project')
     #items = [x for sub in xml for x in sub.getElementsByTagName('project')]
     all_projects = []
@@ -264,22 +264,22 @@ def main():
     args = parse_args()
     default_branch = args.branch
 
-    base_path = os.getenv('SCREWD_CROWDIN_BASE_PATH')
+    base_path = os.getenv('AQUARIOS_CROWDIN_BASE_PATH')
     if base_path is None:
         cwd = os.getcwd()
-        print('You have not set SCREWD_CROWDIN_BASE_PATH. Defaulting to %s' % cwd)
+        print('You have not set AQUARIOS_CROWDIN_BASE_PATH. Defaulting to %s' % cwd)
         base_path = cwd
     else:
         base_path = os.path.join(os.path.realpath(base_path))
     if not os.path.isdir(base_path):
-        print('SCREWD_CROWDIN_BASE_PATH + branch is not a real directory: c'
+        print('AQUARIOS_CROWDIN_BASE_PATH + branch is not a real directory: c'
               % base_path)
         sys.exit(1)
 
     if not check_dependencies():
         sys.exit(1)
 
-    xml_android = load_xml(x='%s/manifest/manifests/screwd.xml' % base_path)
+    xml_android = load_xml(x='%s/manifest/default.xml' % base_path)
     if xml_android is None:
         sys.exit(1)
 
